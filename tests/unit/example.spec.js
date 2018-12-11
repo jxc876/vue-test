fb270cb133638bb194e59d07d2af99a6460324f0
jxc876 <jxc876@gmail.com>
2018-12-11 00:51:38 -0600

    it seems to work

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -3,11 +3,17 @@ import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
+
   it('renders props.msg when passed', () => {
     const msg = 'new message'
+
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
     })
-    expect(wrapper.text()).to.include(msg)
+
+    const renderedText = wrapper.find('span').text()
+    expect(renderedText).to.equal('new message');
+    // expect(wrapper.text()).to.include(msg)
   })
+
 })
